79569ab12c3c3f24e39c0cb8bd917bf521f43f1c
Yuval Adam <yuv.adm@gmail.com>
2013-10-27 02:09:55 +0200

    Initial working version

---
+++ free_haaretz.js
@@ -0,0 +1,22 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'User-Agent') {
+        details.requestHeaders[i].value = 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)';
+        break;
+      }
+    }
+    return {requestHeaders: details.requestHeaders};
+  },
+  {
+    urls: ["http://*.haaretz.co.il/*"]
+  },
+  [
+    "blocking",
+    "requestHeaders"
+  ]);
+
+chrome.contentSettings.javascript.set({
+  primaryPattern: "http://*.haaretz.co.il/*",
+  setting: "block"
+})
