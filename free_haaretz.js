427c71eae14720a18d05f6843d2ca2d04a3e487c
roded <roded@protonmail.com>
2018-06-12 21:38:37 +0300

    Mobile support

--- free_haaretz.js
+++ free_haaretz.js
@@ -1,3 +1,10 @@
+
+// Reference: https://support.google.com/webmasters/answer/1061943?hl=en
+var USER_AGENTS = {
+  DESKTOP: 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
+  MOBILE: 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+};
+
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     var url_parts = details.url.split('?');
@@ -23,7 +30,11 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
   function (details) {
     for (var i = 0; i < details.requestHeaders.length; ++i) {
       if (details.requestHeaders[i].name.toLowerCase() === 'user-agent') {
-        details.requestHeaders[i].value = 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)';
+        if (typeof window.orientation !== 'undefined') {
+          details.requestHeaders[i].value = USER_AGENTS.MOBILE;
+        } else {
+          details.requestHeaders[i].value = USER_AGENTS.DESKTOP;
+        }
         break;
       }
     }
