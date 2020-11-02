c76e84a5e2168a2ae2e016a1bdc405e27445387e
sha-265 <4103710+sha-265@users.noreply.github.com>
2020-11-02 22:05:32 +0200

    Update User-Agent to fix bypass

--- free_haaretz.js
+++ free_haaretz.js
@@ -1,8 +1,8 @@
 
 // Reference: https://support.google.com/webmasters/answer/1061943?hl=en
 var USER_AGENTS = {
-  DESKTOP: 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
-  MOBILE: 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+  DESKTOP: 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
+  MOBILE: 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36 Edg/80.0.345.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'
 };
 
 chrome.webRequest.onBeforeRequest.addListener(
